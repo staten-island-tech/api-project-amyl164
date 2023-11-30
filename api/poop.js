5794b39acd9fc68c8fd5d69f7a0471eded0fa256
amyl164 <amyl81008@gmail.com>
2023-11-30 10:15:29 -0500

    ss

---
+++ api/poop.js
@@ -0,0 +1,21 @@
+// const URL = "https://dog.ceo/api/breeds/image/random";
+// console.log(URL)
+
+const URL =`https://dog.ceo/api/breeds/image/random`;
+
+async function getData(URL){
+    try {
+        //requesting a response RELATIONAL STATE TRANSFER (REST) API'S
+        const response = await fetch(URL);
+        if(response.status !=200){
+            throw new Error(response.statusText);
+        }
+        //convert response to JSON
+        const data = await response.json();
+        document.querySelector("h1").textContent = data.content;
+    } catch (error) {
+        console.log(error,"UH OH SPAGHETTIOS");
+        document.querySelector("h1").textContent =  "Ahh Snap";
+    }
+}
+getData(URL);
