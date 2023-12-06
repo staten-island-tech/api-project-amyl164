12dd9eb1b99c646870b02fe02c2b08730ca3d2cf
amyl164 <amyl81008@gmail.com>
2023-12-06 10:15:23 -0500

    tt

--- api/poop.js
+++ api/poop.js
@@ -1,19 +1,23 @@
 
-const URL =`https://api.quotable.io/random`;
-
-async function getData(URL){
-    try {
-        //requesting a response RELATIONAL STATE TRANSFER (REST) API'S
-        const response = await fetch(URL);
-        if(response.status !=200){
-            throw new Error(response.statusText);
-        }
-        //convert response to JSON
-        const data = await response.json();
-        document.querySelector("h1").textContent = data.content;
-    } catch (error) {
-        console.log(error,"UH OH SPAGHETTIOS");
-        document.querySelector("h1").textContent =  "srgaer";
-    }
+const URL = `https://api.algobook.info/v1/dogs/all` ;
+//goes before anything bfore getting data from api
+async function getData (URL){
+try {
+//requesting response REST 'API'S
+const response = await fetch (URL);
+if (response.status !=200){
+throw new Error (response.statusText);
+//convert response to json
 }
-getData(URL);
+const data = await response. json();
+data.forEach(data => {document.querySelector("h1").textContent = data.name;
+});
+data.forEach(data => {console.log(data.name)
+});
+}
+catch (error) {
+console.log(error, "uhoh")
+document.querySelector("h1").textContent = "no work";
+}
+}
+getData (URL);
