67b50ec814f5a0f38012557f8acf0dfabf9f57f2
amyl164 <amyl81008@gmail.com>
2024-01-04 10:15:50 -0500

    br

--- api/js/poop.js
+++ api/js/poop.js
@@ -17,7 +17,7 @@ DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
     <h2 class="breed">${data.name}</h2>
-    <img src="${data.imgUrl}" alt="" class="dogimg">
+    <img src="${data.imgUrl}" alt="${"image of " +data.name}" class="dogimg">
     <h3 class="personality">${"Temperament: "+data.temperament}</h3>
     <h3 class="lifespan">${"Lifespan: "+data.lifespan}</h3>
     <h3 class="weightlbs">${"Weight(lbs): "+data.weightLbs}</h3>
@@ -42,7 +42,7 @@ function clearfields()
 }
 function clearh2()
 {
-  DOMSelectors.h2.innerHTML = "";
+  DOMSelectors.forh2.innerHTML = "";
 }
 
 DOMSelectors.form.addEventListener("submit", function(event){
@@ -63,7 +63,7 @@ DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
     <h2 class="breed">${data.name}</h2>
-    <img src="${data.imgUrl}" alt="" class="dogimg">
+    <img src="${data.imgUrl}" alt="${"image of " +data.name}" class="dogimg">
     <h3 class="personality">${"Temperament: "+data.temperament}</h3>
     <h3 class="lifespan">${"Lifespan: "+data.lifespan}</h3>
     <h3 class="weightlbs">${"Weight(lbs): "+data.weightLbs}</h3>
@@ -74,7 +74,10 @@ insertCards(data)
 }
 catch (error) {
 console.log(error, "oppsie daisy")
-document.querySelector("h2").textContent = "Check your spelling or the API might not have that dog breed!";
+DOMSelectors.for_h2.insertAdjacentHTML(
+  `afterbegin`,
+  `<h2 class="h2" alt="Different Type of Dog Breeds">Check your spelling or the API might not have that dog breed!</h2>`
+)
 DOMSelectors.form.addEventListener("submit", function(event){
   event.preventDefault();
   clearh2()
@@ -86,6 +89,7 @@ getData(husky)});
 DOMSelectors.button.addEventListener("click", function(event){
   event.preventDefault();
   clearfields()
+  clearh2()
   const reset = `https://api.algobook.info/v1/dogs/all`;
 async function getData (reset){
 try {
@@ -100,7 +104,7 @@ DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
     <h2 class="breed">${data.name}</h2>
-    <img src="${data.imgUrl}" alt="" class="dogimg">
+    <img src="${data.imgUrl}" alt="${"image of " +data.name}" class="dogimg">
     <h3 class="personality">${"Temperament: "+data.temperament}</h3>
     <h3 class="lifespan">${"Lifespan: "+data.lifespan}</h3>
     <h3 class="weightlbs">${"Weight(lbs): "+data.weightLbs}</h3>
